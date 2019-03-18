6273767f10d26e329dbf4ccd24b451f339d2486a
Artur Muniz <munizart_dev@hotmail.com>
2019-03-18 20:13:33 -0300

    docs

--- part-2/src/get.js
+++ part-2/src/get.js
@@ -1,7 +1,17 @@
 const axios = require('axios')
 
+/**
+ * @param { function (T) => U } fn
+ * @template T, U
+ */
 const memoize = (fn) => {
+  /** @type { Map<T, U> } */
   const cache = new Map()
+
+  /**
+   * @param { T }
+   * @returns { U }
+   */
   return (arg) => {
     if (cache.has(arg)) {
       return cache.get(arg)
@@ -12,6 +22,12 @@ const memoize = (fn) => {
     return result
   }
 }
-const memoGet = memoize((url) => axios.get(url))
+const memoGet = memoize(
+  /**
+   * @param { String } url
+   * @return { Promise<AxiosResponse> }
+   */
+  (url) => axios.get(url)
+)
 
 module.exports = memoGet
