4e290b82f34b712ea30fdf83913d08e7296cdda6
Artur Muniz <munizart_dev@hotmail.com>
2019-03-18 20:12:51 -0300

    clining-up aggregator-api server

--- url-aggregator-api.js
+++ url-aggregator-api.js
@@ -3,28 +3,25 @@ const app = express()
 const { performance } = require('perf_hooks')
 
 let hits = 0
-let notFound = 0
 
 app.use((req, res, next) => {
-    req.receivedAt = performance.now()
-    next()
+  req.receivedAt = performance.now()
+  next()
 })
 
 app.get('/images/:img.png', (req, res) => {
-    hits++
-    const { img } = req.params
-    if (parseInt(img, 10) % 5 === 0) {
-        res.status(404).send('There\'s no such image!')
-        notFound++
-    } else {
-        res.send('You got a 200!')
-    }
-    process.nextTick(() => {
-      console.log(`404:\t${notFound}\thits:\t${hits}\t${ performance.now() - req.receivedAt }`)
-    })
+  hits++
+  const { img } = req.params
+  if (parseInt(img, 10) % 5 === 0) {
+    res.status(404).send('There\'s no such image!')
+  } else {
+    res.send('You got a 200!')
+  }
+  process.nextTick(() => {
+    console.log(`hits:\t${hits}`)
+  })
 })
 
 app.listen(4567, () => {
-    console.log('listening at 4567\n')
+  console.log('listening at 4567')
 })
-
